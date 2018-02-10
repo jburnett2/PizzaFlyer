63e160f71f53af90651096de207c159712b58a34
samuelang <samuel.ang.prog@gmail.com>
2018-02-10 12:38:43 -0600

    Added Dispenser

---
+++ Assets/Dispenser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dispenser : MonoBehaviour {
+	public ParticleSystem ps;
+	public float toppingDeployTime = 1;
+	// Use this for initialization
+	void Start () {
+		ps.Stop ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		//Debug.Log ("ENTERED");
+		StartCoroutine(DeployToppings());
+	}
+
+	IEnumerator DeployToppings(){
+		ps.Play ();
+		yield return new WaitForSeconds (toppingDeployTime);
+		ps.Stop();
+	}
+}
