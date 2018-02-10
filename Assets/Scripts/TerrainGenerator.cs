2530040fe9d7b3f0c8bc2345cad5fcbf28e99acc
jburnett2 <jburnett@trinity.edu>
2018-02-10 16:18:41 -0600

    Added Jesse Scene

---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+
+    public int depth = 50;
+    public int width = 256;
+    public int height = 256;
+
+    public float scale = 15f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+    }
+
+    TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, GenerateHeights());
+        return terrainData;
+    }
+
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                heights[x, y] = CalculateHeights(x, y);
+            }
+        }
+        return heights;
+    }
+
+    float CalculateHeights(int x, int y)
+    {
+        float xCoord = (float)x / width * scale;
+        float yCoord = (float)y / (height/2) * scale;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
